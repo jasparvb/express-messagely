f9494e803b927505428c28c7e9c8f3a413b88c10
von Buelow <jaspar@UMICH.EDU>
2020-06-23 23:03:24 -0400

    Login route

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const User = require("../models/user");
+const ExpressError = require("../expressError");
+
 
 const router = new express.Router();
 
@@ -10,6 +12,19 @@ const router = new express.Router();
  * Make sure to update their last-login!
  *
  **/
+router.post("/login", async function(req, res, next) {
+    try {
+        if(await User.authenticate(req.body)) {
+            let token = jwt.sign({username: req.body.username}, SECRET_KEY);
+            User.updateLoginTimestamp(req.body.username);
+            return res.json({ token });
+        } else {
+            throw new ExpressError("Invalid username/password", 401);
+        }
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 
